fccab6b632fb7db8e53b2d581ed46090d40bcd7c
Michael Williams <michaelwilliams@SDMacBookPro2.local>
2016-11-27 11:13:15 -0700

    Added additional hibernate dependencies

--- IAmLegendWeb/src/entities/User.java
+++ IAmLegendWeb/src/entities/User.java
@@ -1,19 +1,5 @@
 package data;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-
-import javax.validation.constraints.DecimalMax;
-import javax.validation.constraints.DecimalMin;
-//import javax.validation.constraints.Digits;
-//import javax.validation.constraints.Max;
-//import javax.validation.constraints.Min;
-//import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
-import org.springframework.stereotype.Repository;
-
 //import org.hibernate.validator.constraints.Email;
 //import org.hibernate.validator.constraints.NotBlank;
 //import org.hibernate.validator.constraints.NotEmpty;
