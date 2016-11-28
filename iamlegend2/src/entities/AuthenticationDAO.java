8683484262527ad8252fcf47be87ddead1522266
Michael Williams <michaelwilliams@SDMacBookPro2.local>
2016-11-28 03:21:15 -0700

    updated URLs for weapons and Customer table

---
+++ iamlegend2/src/entities/AuthenticationDAO.java
@@ -0,0 +1,17 @@
+package entities;
+import java.util.Collection;
+
+public interface AuthenticationDAO {
+	public User createAccount(User user);
+	public Boolean registerEmail(String email);
+	public Boolean registerPassword(String password);
+	public User getUserAccount(User user);
+	public Boolean validEmail(String email);
+	public Boolean validPassword(String password);
+	public void displayUsers();
+	public Collection<User> values();
+	public Collection<Integer> keySet();
+	public Integer[] keyList();
+	public User get(Integer accountNumber);
+
+}
---
+++ iamlegend2/src/entities/Customer.java
@@ -0,0 +1,118 @@
+package entities;
+
+public class Customer implements Comparator<Customer>{
+	
+	iamprivate int id;
+	private int accountBalance;
+	private String email;
+	private String password;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String age;
+	private int height;
+	private int weight;
+	private String accessLevel;
+	private String orderStatus;
+	private String securityQuestion1;
+	private String securityQuestion2;
+	private int zipcode;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public int getAccountBalance() {
+		return accountBalance;
+	}
+	public void setAccountBalance(int accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public int getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public String getAccessLevel() {
+		return accessLevel;
+	}
+	public void setAccessLevel(String accessLevel) {
+		this.accessLevel = accessLevel;
+	}
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	public String getSecurityQuestion1() {
+		return securityQuestion1;
+	}
+	public void setSecurityQuestion1(String securityQuestion1) {
+		this.securityQuestion1 = securityQuestion1;
+	}
+	public String getSecurityQuestion2() {
+		return securityQuestion2;
+	}
+	public void setSecurityQuestion2(String securityQuestion2) {
+		this.securityQuestion2 = securityQuestion2;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", accountBalance=" + accountBalance + ", email=" + email + ", password="
+				+ password + ", firstName=" + firstName + ", middleName=" + middleName + ", lastName=" + lastName
+				+ ", age=" + age + ", zipcode=" + zipcode + ", height=" + height + ", weight=" + weight
+				+ ", accessLevel=" + accessLevel + ", orderStatus=" + orderStatus + ", securityQuestion1="
+				+ securityQuestion1 + ", securityQuestion2=" + securityQuestion2 + "]";
+	}
+	
+}
